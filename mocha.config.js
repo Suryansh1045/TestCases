ae12d05914033b626874c0cb9d8be17bc73eb7ae
Rashika <rashika@perceptsystems.com>
2024-06-04 13:22:41 +0530

    acce

--- mocha.config.js
+++ mocha.config.js
@@ -1,5 +1,6 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const edge = require('selenium-webdriver/edge');
 
 let options = new chrome.Options();
 options.excludeSwitches(['enable-logging']);
@@ -11,6 +12,15 @@ options.addArguments('log-level=3');  // Reduce console log level
 options.addArguments('disable-logging');  // Disable logging
 options.addArguments('output=/dev/null');  // Disable logging
 
+let edgeOptions = new edge.Options();
+edgeOptions.excludeSwitches(['enable-logging']);
+edgeOptions.addArguments('headless');  // Uncomment to run in headless mode
+edgeOptions.addArguments('disable-gpu');  // Applicable for some environments
+edgeOptions.addArguments('disable-dev-shm-usage');  // Overcome limited resource problems
+edgeOptions.addArguments('no-sandbox');  // Bypass OS security model
+edgeOptions.addArguments('log-level=3');  // Reduce console log level
+edgeOptions.addArguments('disable-logging');  // Disable logging
+edgeOptions.addArguments('output=/dev/null');  // Disable logging
 
 const chromeDriver = new Builder()
     .forBrowser("chrome")
@@ -23,5 +33,5 @@ module.exports = {
     chromeDriver,
     Key,
     By,
-    until,Builder
+    until,Builder,edgeDriver
 };
