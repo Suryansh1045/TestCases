3e5e16e8d54c386d2c67f51a71c633b4b14cd744
Rashika <rashika@perceptsystems.com>
2024-06-04 13:54:42 +0530

    a3c

--- mocha.config.js
+++ mocha.config.js
@@ -13,29 +13,17 @@ options.addArguments('log-level=3');  // Reduce console log level
 options.addArguments('disable-logging');  // Disable logging
 options.addArguments('output=/dev/null');  // Disable logging
 
-let edgeOptions = new edge.Options();
-edgeOptions.excludeSwitches(['enable-logging']);
-edgeOptions.addArguments('headless');  // Uncomment to run in headless mode
-edgeOptions.addArguments('disable-gpu');  // Applicable for some environments
-edgeOptions.addArguments('disable-dev-shm-usage');  // Overcome limited resource problems
-edgeOptions.addArguments('no-sandbox');  // Bypass OS security model
-edgeOptions.addArguments('log-level=3');  // Reduce console log level
-edgeOptions.addArguments('disable-logging');  // Disable logging
-edgeOptions.addArguments('output=/dev/null');  // Disable logging
 
 const chromeDriver = new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
     .build();
 
-    const edgeDriver=new  Builder()
-    .forBrowser(Browser.EDGE)
-    .setEdgeOptions(edgeOptions)
-    .build();    
+     
     
 module.exports = {
     chromeDriver,
     Key,
     By,
-    until,Builder,edgeDriver
+    until,Builder
 };
