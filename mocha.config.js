c909dd4478fe7a611858851548d967ae845557ad
Rashika <rashika@perceptsystems.com>
2024-06-04 15:00:43 +0530

    a4c

--- mocha.config.js
+++ mocha.config.js
@@ -1,7 +1,6 @@
-const { Builder, By, Key, until,Browser } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const edge = require('selenium-webdriver/edge');
-
 
 let options = new chrome.Options();
 options.excludeSwitches(['enable-logging']);
@@ -13,17 +12,29 @@ options.addArguments('log-level=3');  // Reduce console log level
 options.addArguments('disable-logging');  // Disable logging
 options.addArguments('output=/dev/null');  // Disable logging
 
+let edgeOptions = new edge.Options();
+edgeOptions.excludeSwitches(['enable-logging']);
+edgeOptions.addArguments('headless');  // Uncomment to run in headless mode
+edgeOptions.addArguments('disable-gpu');  // Applicable for some environments
+edgeOptions.addArguments('disable-dev-shm-usage');  // Overcome limited resource problems
+edgeOptions.addArguments('no-sandbox');  // Bypass OS security model
+edgeOptions.addArguments('log-level=3');  // Reduce console log level
+edgeOptions.addArguments('disable-logging');  // Disable logging
+edgeOptions.addArguments('output=/dev/null');  // Disable logging
 
 const chromeDriver = new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
     .build();
 
-     
+const edgeDriver=new  Builder()
+    .forBrowser("MicrosoftEdge")
+    .setEdgeOptions(edgeOptions)
+    .build();    
     
 module.exports = {
     chromeDriver,
     Key,
     By,
-    until,Builder
+    until,Builder,edgeDriver
 };
--- testCases/FormValidation.js
+++ testCases/FormValidation.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const {chromeDriver, By, Key, until } = require('../mocha.config');
+const {chromeDriver, By, Key, until ,edgeDriver} = require('../mocha.config');
 
 describe("Intial Claims Test Cases using Chrome Browser", function () {
   console.log("first test cases is getting executed");
@@ -54,56 +54,56 @@ describe("Intial Claims Test Cases using Chrome Browser", function () {
  
 });
 
-// describe("Intial Claims Test Cases using Edge Browser",function(){
+describe("Intial Claims Test Cases using Edge Browser",function(){
 
-//   it("TestCase 1: Valid Form Data using Edge Browser", async function () {
-//     await validFormData(edgeDriver,"Test","DBQ01","chadDBQTest01@chadcollins.com", 3653249292,94041);
-//   });
+  it("TestCase 1: Valid Form Data using Edge Browser", async function () {
+    await validFormData(edgeDriver,"Test","DBQ01","chadDBQTest01@chadcollins.com", 3653249292,94041);
+  });
   
-//   it("TestCase 2: Invalid Phone Number using Edge Browser",async function(){
-//     await InvalidPhoneNumber(edgeDriver,"Test","DBQ02","chadDBQTest02@chadcollins.com", 1010101010,96041)
-//   })
+  it("TestCase 2: Invalid Phone Number using Edge Browser",async function(){
+    await InvalidPhoneNumber(edgeDriver,"Test","DBQ02","chadDBQTest02@chadcollins.com", 1010101010,96041)
+  })
   
-//   it("TestCase 3: Invalid Phone Number using Edge Browser",async function(){
-//     await InvalidPhoneNumber(edgeDriver,"Test","DBQ03","chadDBQTest03@chadcollins.com", 3452342344,96041)
-//   })
+  it("TestCase 3: Invalid Phone Number using Edge Browser",async function(){
+    await InvalidPhoneNumber(edgeDriver,"Test","DBQ03","chadDBQTest03@chadcollins.com", 3452342344,96041)
+  })
 
-//   it("TestCase 4: Non Digits Phone Number using Edge Browser",async function(){
-//     await InvalidPhoneNumber(edgeDriver,"Test","DBQ04","chadDBQTest04@chadcollins.com", "asdasddasd",96041)
-//   })
+  it("TestCase 4: Non Digits Phone Number using Edge Browser",async function(){
+    await InvalidPhoneNumber(edgeDriver,"Test","DBQ04","chadDBQTest04@chadcollins.com", "asdasddasd",96041)
+  })
 
-//   it("TestCase 5: Special Characters using Edge Browser",async function(){
-//     await InvalidPhoneNumber(edgeDriver,"Test","DBQ05","chadDBQTest05@chadcollins.com", 345-345-3455,96041)
-//   })
+  it("TestCase 5: Special Characters using Edge Browser",async function(){
+    await InvalidPhoneNumber(edgeDriver,"Test","DBQ05","chadDBQTest05@chadcollins.com", 345-345-3455,96041)
+  })
 
-//   it("TestCase 6: Special Characters using Edge Browser",async function(){
-//     await InvalidPhoneNumber(edgeDriver,"Test","DBQ06","chadDBQTest06@chadcollins.com","123123123#",96041)
-//   })
+  it("TestCase 6: Special Characters using Edge Browser",async function(){
+    await InvalidPhoneNumber(edgeDriver,"Test","DBQ06","chadDBQTest06@chadcollins.com","123123123#",96041)
+  })
 
-//   it("TestCase 7: valid Phone Number using Edge Browser",async function(){
-//     await validFormData(edgeDriver,"Test","DBQ07","chadDBQTest07@chadcollins.com",2403753633,96041)
-//   })
+  it("TestCase 7: valid Phone Number using Edge Browser",async function(){
+    await validFormData(edgeDriver,"Test","DBQ07","chadDBQTest07@chadcollins.com",2403753633,96041)
+  })
 
-//   it("TestCase 8: valid Phone Number using Edge Browser",async function(){    
-//     await validFormData(edgeDriver,"Test","DBQ08","chadDBQTest08@chadcollins.com",2403985633,96041)
-//   })
+  it("TestCase 8: valid Phone Number using Edge Browser",async function(){    
+    await validFormData(edgeDriver,"Test","DBQ08","chadDBQTest08@chadcollins.com",2403985633,96041)
+  })
 
-//   it("TestCase 9: Already registered number Phone Number using Edge Browser",async function(){
-//     await InvalidPhoneNumber(edgeDriver,"Test","DBQ09","chadDBQTest09@chadcollins.com", 8645823654,96041,true)
-//   })
+  it("TestCase 9: Already registered number Phone Number using Edge Browser",async function(){
+    await InvalidPhoneNumber(edgeDriver,"Test","DBQ09","chadDBQTest09@chadcollins.com", 8645823654,96041,true)
+  })
 
-//   it("TestCase 10: Invalid Zip Code using Edge Browser",async function(){
-//     await InvalidZip(edgeDriver,"Test","DBQ10","chadDBQTest10@chadcollins.com",5158451155 ,940)
-//   })
+  it("TestCase 10: Invalid Zip Code using Edge Browser",async function(){
+    await InvalidZip(edgeDriver,"Test","DBQ10","chadDBQTest10@chadcollins.com",5158451155 ,940)
+  })
 
-//   it("TestCase 11: Invalid Zip Code using Edge Browser",async function(){
-//     await InvalidZip(edgeDriver,"Test","DBQ11","chadDBQTest11@chadcollins.com",5158451155 ,"940@1")
-//   })
+  it("TestCase 11: Invalid Zip Code using Edge Browser",async function(){
+    await InvalidZip(edgeDriver,"Test","DBQ11","chadDBQTest11@chadcollins.com",5158451155 ,"940@1")
+  })
 
-//   it("TestCase 12: Valid Zip Code using Edge Browser",async function(){
-//     await validFormData(edgeDriver,"Test","DBQ12","chadDBQTest12@chadcollins.com",5158451155 ,94041)
-//   })
-// });
+  it("TestCase 12: Valid Zip Code using Edge Browser",async function(){
+    await validFormData(edgeDriver,"Test","DBQ12","chadDBQTest12@chadcollins.com",5158451155 ,94041)
+  })
+});
 
 const validFormData=async(driver,firstname,lastname,email,phoneNumber,zip)=>
 {
